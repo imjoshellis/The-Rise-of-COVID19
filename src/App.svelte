e52cce82e03da4e39939d556f079ea5d6b719d21
imjoshellis <joshe523@gmail.com>
2020-03-17 11:31:20 -0500

    Start parsing API data

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,10 @@
 <script>
   import Tailwindcss from './Tailwindcss.svelte'
-  import FusionCharts from 'fusioncharts'
-  import Charts from 'fusioncharts/fusioncharts.charts'
-  import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
   import SvelteFC, { fcRoot } from 'svelte-fusioncharts'
   import { onMount } from 'svelte'
 
   const apiURL = 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats'
-  let data = []
+  let chartData = []
 
   onMount(async function() {
     const response = await fetch(apiURL, {
@@ -16,42 +13,17 @@
         'x-rapidapi-key': '5678e57a0dmsh83ea7d4f23d48e6p1152d7jsn678f6668471c'
       }
     })
+
+    let data = []
     data = await response.json()
+    data.data.covid19Stats.forEach(o => {
+      chartData.push({ label: o.country, value: o.confirmed })
+    })
     console.log(data)
+    console.log(data.data.covid19Stats)
+    console.log(chartData)
   })
-
-  // Always set FusionCharts as the first parameter
-  fcRoot(FusionCharts, Charts, FusionTheme)
-  const dataSource = {
-    chart: {
-      caption: 'Countries With Most Oil Reserves [2017-18]',
-      subCaption: 'In MMbbl = One Million barrels',
-      xAxisName: 'Country',
-      yAxisName: 'Reserves (MMbbl)',
-      numberSuffix: 'K',
-      theme: 'fusion'
-    },
-    data: [
-      { label: 'Venezuela', value: '290' },
-      { label: 'Saudi', value: '260' },
-      { label: 'Canada', value: '180' },
-      { label: 'Iran', value: '140' },
-      { label: 'Russia', value: '115' },
-      { label: 'UAE', value: '100' },
-      { label: 'US', value: '30' },
-      { label: 'China', value: '30' }
-    ]
-  }
-
-  const chartConfigs = {
-    type: 'column2d',
-    width: 600,
-    height: 400,
-    dataFormat: 'json',
-    dataSource: dataSource
-  }
 </script>
 
 <Tailwindcss />
 <h1 class="text-red-500">Testing Tailwind</h1>
-<SvelteFC {...chartConfigs} />
