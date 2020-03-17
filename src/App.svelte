888f8755325aa0771602f65a4bece141f05bf1d7
imjoshellis <joshe523@gmail.com>
2020-03-17 11:46:50 -0500

    Check otu chart.js

--- src/App.svelte
+++ src/App.svelte
@@ -6,23 +6,23 @@
   const apiURL = 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats'
   let chartData = []
 
-  onMount(async function() {
-    const response = await fetch(apiURL, {
-      headers: {
-        'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
-        'x-rapidapi-key': '5678e57a0dmsh83ea7d4f23d48e6p1152d7jsn678f6668471c'
-      }
-    })
+  // onMount(async function() {
+  //   const response = await fetch(apiURL, {
+  //     headers: {
+  //       'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+  //       'x-rapidapi-key': '5678e57a0dmsh83ea7d4f23d48e6p1152d7jsn678f6668471c'
+  //     }
+  //   })
 
-    let data = []
-    data = await response.json()
-    data.data.covid19Stats.forEach(o => {
-      chartData.push({ label: o.country, value: o.confirmed })
-    })
-    console.log(data)
-    console.log(data.data.covid19Stats)
-    console.log(chartData)
-  })
+  //   let data = []
+  //   data = await response.json()
+  //   data.data.covid19Stats.forEach(o => {
+  //     chartData.push({ label: o.country, value: o.confirmed })
+  //   })
+  //   console.log(data)
+  //   console.log(data.data.covid19Stats)
+  //   console.log(chartData)
+  // })
 </script>
 
 <Tailwindcss />
