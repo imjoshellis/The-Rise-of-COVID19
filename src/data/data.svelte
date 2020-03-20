3ef03be6f84effcf1df661d82e24f6e1bad484cd
imjoshellis <joshe523@gmail.com>
2020-03-19 21:37:08 -0500

    Setup async storage of data

--- src/data/data.svelte
+++ src/data/data.svelte
@@ -1,6 +1,11 @@
 <script>
   import getApi from './data.js'
-  import { data } from './stores.js'
+  import { data, dates, dateMax, dateIdx } from './stores.js'
 
-  $: (async () => ($data = await getApi()))()
+  $: (async () => {
+    $data = await getApi()
+    $dates = await Object.keys($data.confirmed)
+    $dateMax = (await $dates.length) - 1
+    $dateIdx = $dateMax
+  })()
 </script>
--- src/data/stores.js
+++ src/data/stores.js
@@ -1,18 +1,21 @@
-import { derived, readable, writable } from 'svelte/store'
+import { derived, writable } from 'svelte/store'
+
+export const data = writable({})
+export const dates = writable([])
 
 export const regionStr = writable('')
-export const regions = derived(regionStr, $regionStr => {
+export const regions = derived([regionStr, data], ([$regionStr, $data]) => {
   if ($regionStr === '') {
-    return Object.keys(data.countries).sort()
+    return Object.keys($data.countries).sort()
   }
 
   var re = new RegExp($regionStr, 'gi')
 
-  return Object.keys(data.countries)
+  return Object.keys($data.countries)
     .filter(word => word.match(re))
     .sort()
 })
 
-export const dateMax = readable(dates.length - 1)
-export const dateIdx = writable(dates.length - 1)
+export const dateMax = writable(0)
+export const dateIdx = writable(0)
 export const dateValue = derived(dateIdx, $dateIdx => dates[$dateIdx])
