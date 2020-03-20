229b943a1c9b28876c1a4068c24007e833962aa8
imjoshellis <joshe523@gmail.com>
2020-03-20 11:37:54 -0500

    Add links

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,12 +9,18 @@
 </style>
 
 <header>
-  <div>
-    <RegionFilter />
-  </div>
-
+  <nav>
+    <ul class="flex flex-row">
+      <li on:press={reset}>Reset</li>
+      <li on:press={about}>About</li>
+      <li on:press={contribute}>Contribute on GitHub</li>
+    </ul>
+  </nav>
+  <RegionFilter />
   <a href="https://ko-fi.com/T6T31IWGF" target="_blank">
-    <button class="text-sm rounded bg-gray-700 transition-all duration-300 ease-out hover:bg-gray-500 px-4 py-2">
+    <button
+      class="text-sm rounded bg-gray-700 transition-all duration-300 ease-out hover:bg-gray-500 px-4 py-2
+      whitespace-no-wrap">
       ☕ Buy me a coffee →
     </button>
   </a>
--- src/components/RegionFilter.svelte
+++ src/components/RegionFilter.svelte
@@ -1,10 +1,19 @@
 <script>
   import { countryStr } from '../data/stores.js'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    feather.replace()
+  })
 </script>
 
 <style type="text/css">
+  div {
+    overflow: hidden;
+    @apply bg-gray-800 text-gray-200 rounded;
+  }
   input[type='text'] {
-    @apply bg-gray-800 text-gray-200 px-4 py-2 w-64 mx-auto border-none;
+    @apply bg-gray-800 text-gray-200 text-sm pr-4 pl-2 py-2 w-full mx-auto;
   }
 
   input[type='text']:focus {
@@ -12,4 +21,11 @@
   }
 </style>
 
-<input placeholder="Filter Countries by Name" type="text" name="region" id="region" bind:value={$countryStr} />
+<div class="flex flex-row justify-center w-full mx-4">
+  <div class="flex flex-row py-2 pl-2 items-center justify-center text-gray-300">
+    <label for="country">
+      <i data-feather="search" style="height:1.25rem;width:1.25rem;" />
+    </label>
+  </div>
+  <input placeholder="Filter Countries by Name" type="text" name="country" id="country" bind:value={$countryStr} />
+</div>
