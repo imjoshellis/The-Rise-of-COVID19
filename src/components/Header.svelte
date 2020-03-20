72b2bbb894ca83b1f61d2048e051f4a70289c2db
imjoshellis <joshe523@gmail.com>
2020-03-20 11:21:12 -0500

    Style header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,7 +4,7 @@
 
 <style type="text/scss">
   header {
-    @apply flex flex-row;
+    @apply flex flex-row justify-between items-center py-4;
   }
 </style>
 
@@ -14,12 +14,8 @@
   </div>
 
   <a href="https://ko-fi.com/T6T31IWGF" target="_blank">
-    <button class="rounded bg-blue-400 px-4 py-2">☕ Buy me a coffee</button>
-    <img
-      height="36"
-      style="border:0px;height:36px;"
-      src="https://az743702.vo.msecnd.net/cdn/kofi1.png?v=2"
-      border="0"
-      alt="Buy Me a Coffee at ko-fi.com" />
+    <button class="text-sm rounded bg-gray-700 transition-all duration-300 ease-out hover:bg-gray-500 px-4 py-2">
+      ☕ Buy me a coffee →
+    </button>
   </a>
 </header>
