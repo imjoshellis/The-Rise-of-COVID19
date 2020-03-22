2c8e949848855cb808dd9a26768779cb1b0e9194
imjoshellis <joshe523@gmail.com>
2020-03-22 16:03:25 -0500

    Setup area component

--- src/components/Area/AreaContainer.svelte
+++ src/components/Area/AreaContainer.svelte
@@ -2,11 +2,30 @@
   import Area from './Area.svelte'
   import SubAreaGrid from '../SubArea/SubAreaGrid.svelte'
   import SquareGrid from './Square/SquareGrid.svelte'
+  import { area } from '../../data/stores.js'
+  import { colorizeText } from '../../data/data.js'
+  const arrow = (x, y) => (x > y ? '▴' : x === y ? '·' : '▾')
+  let p
+  $: p = $area
+
+  $: p.nowRate = (p.today / p.yesterday).toPrecision(3)
+  $: p.prevRate = (p.yesterday / p.twoAgo).toPrecision(3)
+  $: p.prevprevRate = (p.twoAgo / p.threeAgo).toPrecision(3)
+
+  $: p.nowRateArrow = arrow(p.nowRate, p.prevRate)
+  $: p.prevRateArrow = arrow(p.prevRate, p.prevprevRate)
+
+  $: p.deltaRate = (p.nowRate / p.prevRate).toPrecision(3)
+  $: p.prevDeltaRate = (p.prevRate / p.prevprevRate).toPrecision(3)
+
+  $: p.color = colorizeText(p.nowRate, p.prevRate, p.today)
+
+  $: valid = p.nowRate > 0 && p.nowRate !== Infinity && p.prevRate !== Infinity && p.yesterday > 0 && p.twoAgo > 0
 </script>
 
 <div class="px-16">
   <div class="my-8">
-    <Area />
+    <Area {p} />
   </div>
   <SquareGrid />
   <SubAreaGrid />
