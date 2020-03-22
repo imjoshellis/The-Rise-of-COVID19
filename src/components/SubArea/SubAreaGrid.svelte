53df6aa09d1a1b811964e50a8577d89e4496dd9c
imjoshellis <joshe523@gmail.com>
2020-03-22 15:04:57 -0500

    Switch to global Subareas

--- src/components/SubArea/SubAreaGrid.svelte
+++ src/components/SubArea/SubAreaGrid.svelte
@@ -1,13 +1,11 @@
 <script>
   import SubAreaContainer from './SubAreaContainer.svelte'
   import { last } from '../../data/data.js'
-  import { dateIdx, data, dates } from '../../data/stores.js'
-
-  export let subAreas // [{ name: '', today: '', yesterday: '', twoAgo: '', threeAgo: '' }]
+  import { subAreas, dateIdx, data, dates } from '../../data/stores.js'
 </script>
 
 <div class="grid lg:grid-cols-3 gap-4">
-  {#each subAreas as subArea}
+  {#each $subAreas as subArea}
     <SubAreaContainer {subArea} />
   {/each}
 </div>
