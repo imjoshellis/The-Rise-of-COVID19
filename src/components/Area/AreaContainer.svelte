bb99c8c3616758396923dfe5d409d82da1b54fdd
imjoshellis <joshe523@gmail.com>
2020-03-22 13:33:07 -0500

    Resolve errors so page loads

--- src/components/Area/AreaContainer.svelte
+++ src/components/Area/AreaContainer.svelte
@@ -4,13 +4,14 @@
   import SubAreaGrid from '../SubArea/SubAreaGrid.svelte'
   import SquareGrid from './Square/SquareGrid.svelte'
 
-  const recent = last($area.activeList.slice(0, $dateIdx), 4)
-  for (const a in $area.subAreas) {
-    $area.a.today = recent[3]
-    $area.a.yesterday = recent[2]
-    $area.a.twoAgo = recent[1]
-    $area.a.threeAgo = recent[0]
-  }
+  // const recent = last($area.activeList.slice(0, $dateIdx), 4)
+  // for (const a in $area.subAreas) {
+  //   $area.a.today = recent[3]
+  //   $area.a.yesterday = recent[2]
+  //   $area.a.twoAgo = recent[1]
+  //   $area.a.threeAgo = recent[0]
+  // }
+  const subAreas = []
 </script>
 
 <div class="px-16">
