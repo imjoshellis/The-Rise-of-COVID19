2bbe52aaeb03cfc69bddb0115e7e0a7a5d16b30e
imjoshellis <joshe523@gmail.com>
2020-03-22 14:55:42 -0500

    Setup global subarea in store

--- src/data/stores.js
+++ src/data/stores.js
@@ -1,9 +1,10 @@
+import moment from 'moment'
 import { derived, writable } from 'svelte/store'
 
 export const data = writable({})
 export const dates = writable([])
 
-export const area = writable({})
+export const area = writable('global')
 
 export const countryStr = writable('')
 export const allCountries = writable({})
@@ -22,3 +23,39 @@ export const countries = derived([countryStr, data], ([$countryStr, $data]) => {
 export const dateMax = writable(0)
 export const dateIdx = writable(0)
 export const dateValue = derived([dates, dateIdx], ([$dates, $dateIdx]) => new Date($dates[$dateIdx]))
+
+export const subAreas = derived([data, area, dateValue], ([$data, $area, $dateValue]) => {
+  const subAreas = []
+  if ($area === 'global') {
+    for (const country in $data.countries) {
+      const today = moment($dateValue)
+        .utc()
+        .format()
+      const todayActive = $data.countries[country].total.active[today]
+      const yesterday = moment($dateValue)
+        .subtract(1, 'days')
+        .utc()
+        .format()
+      const yesterdayActive = $data.countries[country].total.active[yesterday]
+      const twoAgo = moment($dateValue)
+        .subtract(2, 'days')
+        .utc()
+        .format()
+      const twoActive = $data.countries[country].total.active[twoAgo]
+      const threeAgo = moment($dateValue)
+        .subtract(3, 'days')
+        .utc()
+        .format()
+      const threeActive = $data.countries[country].total.active[threeAgo]
+      const subArea = {
+        name: country,
+        today: todayActive,
+        yesterday: yesterdayActive,
+        twoAgo: twoActive,
+        threeAgo: threeActive
+      }
+      subAreas.push(subArea)
+    }
+  }
+  return subAreas
+})
