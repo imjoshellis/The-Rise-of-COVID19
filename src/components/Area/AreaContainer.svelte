93d288eadafcf4a559c3baad7d84282b325bf643
imjoshellis <joshe523@gmail.com>
2020-03-22 15:04:41 -0500

    Refactor to global subareas

--- src/components/Area/AreaContainer.svelte
+++ src/components/Area/AreaContainer.svelte
@@ -1,17 +1,7 @@
 <script>
-  import { area, dateIdx } from '../../data/stores.js'
   import Area from './Area.svelte'
   import SubAreaGrid from '../SubArea/SubAreaGrid.svelte'
   import SquareGrid from './Square/SquareGrid.svelte'
-
-  // const recent = last($area.activeList.slice(0, $dateIdx), 4)
-  // for (const a in $area.subAreas) {
-  //   $area.a.today = recent[3]
-  //   $area.a.yesterday = recent[2]
-  //   $area.a.twoAgo = recent[1]
-  //   $area.a.threeAgo = recent[0]
-  // }
-  const subAreas = []
 </script>
 
 <div class="px-16">
@@ -19,5 +9,5 @@
     <Area />
   </div>
   <SquareGrid />
-  <SubAreaGrid {subAreas} />
+  <SubAreaGrid />
 </div>
