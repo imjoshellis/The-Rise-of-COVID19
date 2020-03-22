33c40bdad5aed925740f36bec4b50dbd475ed3b2
imjoshellis <joshe523@gmail.com>
2020-03-22 15:49:44 -0500

    Make subAreas reactive

--- src/components/Area/Square/SquareContainer.svelte
+++ src/components/Area/Square/SquareContainer.svelte
@@ -2,12 +2,13 @@
   import Square from './Square.svelte'
   import { colorizeBg } from '../../../data/data.js'
   export let subArea
-  let p = { ...subArea }
+  let p
+  $: p = { ...subArea }
 
-  p.nowRate = (p.today / p.yesterday).toPrecision(3)
-  p.prevRate = (p.yesterday / p.twoAgo).toPrecision(3)
+  $: p.nowRate = (p.today / p.yesterday).toPrecision(3)
+  $: p.prevRate = (p.yesterday / p.twoAgo).toPrecision(3)
 
-  p.color = colorizeBg(p.nowRate, p.prevRate, p.today)
+  $: p.color = colorizeBg(p.nowRate, p.prevRate, p.today)
 </script>
 
 <Square {p} />
