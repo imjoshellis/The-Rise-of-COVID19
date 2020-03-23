af720009ed1fb902e71c3e34f9c49b156369a7d6
imjoshellis <joshe523@gmail.com>
2020-03-22 20:42:00 -0500

    Add logic for pinnedAreas

--- src/components/Area/Square/SquareContainer.svelte
+++ src/components/Area/Square/SquareContainer.svelte
@@ -1,14 +1,22 @@
 <script>
   import Square from './Square.svelte'
   import { colorizeBg } from '../../../data/data.js'
+  import { pinnedAreas } from '../../../data/stores.js'
   export let subArea
   let p
+  let pinColor = ''
   $: p = { ...subArea }
 
   $: p.nowRate = (p.today / p.yesterday).toPrecision(3)
   $: p.prevRate = (p.yesterday / p.twoAgo).toPrecision(3)
 
   $: p.color = colorizeBg(p.nowRate, p.prevRate, p.today)
+
+  const pinArea = () => {
+    p.pinned = true
+    pinColor = 'bg-gray-100 w-full h-full opacity-75'
+    $pinnedAreas = [p, ...$pinnedAreas]
+  }
 </script>
 
-<Square {p} />
+<Square {p} {pinColor} {pinArea} />
