dac4839dd75c6da3bcf8326c49033049ea8eb672
imjoshellis <joshe523@gmail.com>
2020-03-23 08:29:49 -0500

    Fix pin color to be dependent on pinned area list

--- src/components/Area/Square/SquareContainer.svelte
+++ src/components/Area/Square/SquareContainer.svelte
@@ -2,9 +2,10 @@
   import Square from './Square.svelte'
   import { colorizeBg } from '../../../data/data.js'
   import { pinnedAreasList } from '../../../data/stores.js'
+  import { beforeUpdate } from 'svelte'
   export let subArea
   let p
-  let pinColor = ''
+  export let pinColor = ''
   $: p = { ...subArea }
 
   $: p.nowRate = (p.today / p.yesterday).toPrecision(3)
@@ -12,7 +13,16 @@
 
   $: p.color = colorizeBg(p.nowRate, p.prevRate, p.today)
 
-  $: pinColor = pinnedAreasList.length > 0 ? '' : pinColor
+  beforeUpdate(() => {
+    let arr = $pinnedAreasList.filter(pinnedAreaName => {
+      return pinnedAreaName === p.name
+    })
+    if (arr.length > 0) {
+      pinColor = 'bg-gray-100 w-full h-full opacity-75'
+    } else {
+      pinColor = ''
+    }
+  })
 
   const pinArea = () => {
     let arr = $pinnedAreasList.filter(pinnedAreaName => {
@@ -22,9 +32,7 @@
       $pinnedAreasList = $pinnedAreasList.filter(pinnedAreaName => {
         return pinnedAreaName !== p.name
       })
-      pinColor = ''
     } else {
-      pinColor = 'bg-gray-100 w-full h-full opacity-75'
       $pinnedAreasList = [p.name, ...$pinnedAreasList]
     }
   }
